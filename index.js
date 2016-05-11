89d09cf31e297187631bbb1ac9a7b4d914e9606e
Chinh Nguyen <chinh7@me.com>
2016-05-11 14:07:51 +0700

    update for jbit

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 $(document).ready(function() {
   var button = $('#login');
-  var user_id = "7605",
+  var quoineHost = "https://sandbox.quoine.com",
+      jbitsHost = "https://jbit.quoine.com";
+  var userId = "7605",
       secret = "/1HwnL1u/DzodLIXmDXYvBtmB7inr/xqKg+F8J8QKVQR7J9pVOg0boH9Lm9eFHyy9h8rfQI7h6W3vdOcL7TWrQ==";
-  console.log(sign(getAuthPayload(user_id), secret));
+
+  //Sign the request for Authentication
   $.ajaxSetup({
     beforeSend: function(xhr) {
-      xhr.setRequestHeader('X-Quoine-Auth', sign(getAuthPayload(user_id), secret));
+      xhr.setRequestHeader('X-Quoine-Auth', sign(getAuthPayload(userId), secret));
     }
   });
+  //end
 
   button.click(function(){
     button.text('Logging in ...');
     button.prop('disabled', true);
-    $.getJSON("https://sandbox.quoine.com/cors_auth", function(response) {
-      console.log(response);
-      window.location = "https://beta-stag.quoine.com/app/#/app/direct-auth?token="+response.encrypted
+    //Request login token
+    $.getJSON(quoineHost + "/cors_auth", function(response) {
+      //Use token to login to trading dashboard
+      window.location = jbitsHost + "/app/#/app/direct-auth?token="+response.encrypted
     });
   });
-
 });
 
 function sign(payload, secret){
@@ -27,9 +31,9 @@ function sign(payload, secret){
   return KJUR.jws.JWS.sign("HS256", sHeader, sPayload, secret);
 }
 
-function getAuthPayload(user_id){
+function getAuthPayload(userId){
   return {
-    user_id: user_id,
+    user_id: userId,
     path: "/cors_auth",
     nonce: new Date().getTime()
   }
